3cdb051fbf838d3bc8ff7d1f00e3d11b161643b0
2020000319 <isy0309@naver.com>
2024-07-10 00:32:09 +0900

    연극/뮤지컬 백부분 구현

--- src/main/java/syim/reviewboard/controller/TheaterApiController.java
+++ src/main/java/syim/reviewboard/controller/TheaterApiController.java
@@ -1,4 +1,24 @@
 package syim.reviewboard.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import syim.reviewboard.model.Theater;
+import syim.reviewboard.service.TheaterService;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/theater/api")
 public class TheaterApiController {
+    private final TheaterService theaterService;
+    public TheaterApiController(TheaterService theaterService){this.theaterService = theaterService;}
+
+    @PostMapping("/search")
+    public List<Theater> apiSearch(@RequestBody Map<String, String> request) {
+        String keyword = request.get("keyword");
+        return theaterService.getTheaterFromApi(keyword);
+    }
 }
--- src/main/java/syim/reviewboard/controller/TheaterController.java
+++ src/main/java/syim/reviewboard/controller/TheaterController.java
@@ -1,4 +1,31 @@
 package syim.reviewboard.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import syim.reviewboard.model.Theater;
+import syim.reviewboard.service.TheaterService;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/Theater")
 public class TheaterController {
+    private TheaterService theaterService;
+    public TheaterController(TheaterService theaterService){this.theaterService = theaterService;}
+    @GetMapping("/search")
+    public String TheaterSearch(Model model) {
+        model.addAttribute("keyword", "");
+        return "board/theaterSearch";
+    }
+
+    @PostMapping("/search")
+    public String search(@ModelAttribute("keyword") String keyword, Model model) {
+        List<Theater> theaterList = theaterService.getTheaterFromApi(keyword);
+        model.addAttribute("theaterList", theaterList);
+        return "board/theaterSearchResult";
+    }
 }
